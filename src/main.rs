0d3d8acadf49420eca6dfa3b26befab83b4ccbd9
shu.yamaga <s.yamaga.0318@gmail.com>
2025-04-16 02:07:15 +0000

    [update] rewrite with clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
-
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
